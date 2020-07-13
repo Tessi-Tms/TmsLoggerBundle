e3017f45d6c97217c12bcc9b268323ad0b440a25
nabil <nabil.mansouri@tessi.fr>
2020-07-13 15:33:15 +0200

    Add get logs extension method

--- Twig/LoggerExtension.php
+++ Twig/LoggerExtension.php
@@ -4,14 +4,31 @@ namespace Tms\Bundle\LoggerBundle\Twig;
 
 use Tms\Bundle\LoggerBundle\Entity\Log;
 use Tms\Bundle\LoggerBundle\Logger\LoggableInterface;
+use Tms\Bundle\LoggerBundle\Logger\LoggerInterface;
 
 class LoggerExtension extends \Twig_Extension
 {
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $loggerManager
+     */
+    public function __construct(LoggerInterface $loggerManager)
+    {
+        $this->loggerManager = $loggerManager;
+    }
+
     public function getFunctions()
     {
         return array(
             new \Twig_SimpleFunction('tms_logger_hash', array($this, 'getHash')),
+            new \Twig_SimpleFunction('tms_logger_logs', array($this, 'getLogs')),
         );
+    }
+
+    public function getLogs(LoggableInterface $loggable)
+    {
+        return $this->loggerManager->getLogs($loggable);
     }
 
     public function getHash(LoggableInterface $loggable)
